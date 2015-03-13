02a1f319845e464065f6dc21264142802396f3c0
nicolasvelah <nicolasvelah@gmail.com>
2015-03-13 16:05:19 -0500

    angular cart
    integracion con angular js

--- app/views/ecommerce/categoria.blade.php
+++ app/views/ecommerce/categoria.blade.php
@@ -1,10 +1,28 @@
 @extends('layout')
 
+@section('scripts')
+
+{{HTML::script('https://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js')}}
+{{HTML::script('dist/ngCart.js')}}
+{{HTML::script('js/cart.js')}}
+
+@endsection
+
 @section('content')
+<div ng-app="CarritoApp">
     @foreach($productos as $prod)
-	    <div>
+	    <div class="producto">
 	        <h1>{{$prod->titulo}}</h1>
+	        <div class="description">
+	        	{{$prod->description}}
+	        </div>
+	        <div class="costo">
+	        	{{$prod->costo}}
+	        </div>
+	        <div class="tools">
+	        	<ngcart-addtocart id="{{ $prod->id }}" name="{{ $prod->titulo }}" price="{{ $prod->costo }}" quantity="1" quantity-max="5" data="item"></ngcart-addtocart>
+	        </div>
 	    </div>
 	@endforeach
-
+</div>
 @stop
--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -14,5 +14,7 @@
 
 @yield('content')
 
+@yield('scripts')
+
 </body>
 </html>
